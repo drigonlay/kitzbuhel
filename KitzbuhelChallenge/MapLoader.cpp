115a657fe5b0b0369cc7c057987ef194a1f17538
drigonlay <louizze.caylan@gmail.com>
2020-06-13 03:53:33 +0800

    Initial files

---
+++ KitzbuhelChallenge/MapLoader.cpp
@@ -0,0 +1,6 @@
+#include "MapLoader.hpp"
+
+void MapLoader::LoadMapFile()
+{
+	std::cout << "Loading map file ...\n" << std::endl;
+}
---
+++ KitzbuhelChallenge/MapLoader.hpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+class MapLoader
+{
+public:
+	MapLoader()
+	{	
+		std::cout << "Hello World!\n";
+	}
+
+	void LoadMapFile();
+};
